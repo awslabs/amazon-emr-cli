68d90aa3e378719e0c2bba7f911195f9762d696b
Damon P. Cortesi <d.lifehacker@gmail.com>
2023-03-16 15:52:35 -0700

    Update tmp path for tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -5,17 +5,20 @@ from emr_cli.packaging.python_project import PythonProject
 class TestInit:
     def test_default_init(self, tmp_path):
         p = PythonProject()
-
-        p.initialize(tmp_path)
-        assert (tmp_path / "pyproject.toml").exists()
-        assert (tmp_path / "entrypoint.py").exists()
-        assert (tmp_path / "jobs" / "extreme_weather.py").exists()
-        assert not (tmp_path / "README.md").exists()
+        target_path = tmp_path / "python_proj"
+        p.initialize(target_path)
+        assert (target_path / "pyproject.toml").exists()
+        assert (target_path / "entrypoint.py").exists()
+        assert (target_path / "jobs" / "extreme_weather.py").exists()
+        assert not (target_path / "README.md").exists()
 
     def test_poetry_init(self, tmp_path):
         p = PythonPoetryProject()
+        target_path = tmp_path / "python_proj"
+        p.initialize(target_path)
+        assert (target_path / "entrypoint.py").exists()
+        assert (target_path / "pyproject.toml").exists()
+        assert (target_path / "README.md").exists()
 
-        p.initialize(tmp_path)
-        assert (tmp_path / "entrypoint.py").exists()
-        assert (tmp_path / "pyproject.toml").exists()
-        assert (tmp_path / "README.md").exists()
+    def test_create_in_existing_folder(self, tmp_path):
+        pass
